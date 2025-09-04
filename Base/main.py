ea8e097eeb8dc07c1c95048f80672a9fa3090163
Diego Arturo García Mondragón <194155-4@iberoleon.edu.mx>
2025-09-04 11:28:53 -0600

    Programa Principal

---
+++ Base/main.py
@@ -0,0 +1,21 @@
+from Clases.operaciones import Operaciones
+
+def main():
+    op = Operaciones()
+    op.leerNumeros()
+    op.sumar()
+    op.mostrarResultado()
+    op.restar()
+    op.mostrarResultado()
+    op.multiplicar()
+    op.mostrarResultado()
+    op.dividir()
+    op.mostrarResultado()
+    op.modulo()
+    op.mostrarResultado()
+    
+    
+if __name__ == "__main__":
+    main()
+    
+    
